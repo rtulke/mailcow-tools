31ebadbaae5a793f6effc2ed39c49fb35653dc81
Robert Tulke <rt@debian.sh>
2018-10-19 02:28:45 +0200

    Update sender_login_maps_update.sh

--- sender_login_maps_update.sh
+++ sender_login_maps_update.sh
@@ -3,9 +3,24 @@
 ####
 # allows the sender and return address to be different
 #
-# vim data/conf/postfix/main.cf
+# prepare sender_login_maps e.g.:
+#   vim /root/dev/mailcow-dockerized/data/conf/postfix/main.cf
 #   replace:  smtpd_sender_login_maps = proxy:mysql:/opt/postfix/conf/sql/mysql_virtual_sender_acl.cf
 #   with:     smtpd_sender_login_maps = hash:/opt/postfix/conf/sender_login_maps, proxy:mysql:/opt/postfix/conf/sql/mysql_virtual_sender_acl.cf
+
+MPATH="/root/dev/mailcow-dockerized"        ## change here your mailcow-dockerized install path
+MCONF="mailcow.conf"
+
+MYPATH=$(pwd)
+
+## test path/file
+if [ ! -f "${MPATH}/${MCONF}" ] ; then
+    echo "file or directory not found, please verify the MPATH= and MCONF= variable";
+    exit 1
+fi
+
+cd $MPATH
+source $MCONF
 
 sender_login_maps="./data/conf/postfix/sender_login_maps"
 sr=${sender_login_maps##*/}
@@ -41,4 +56,7 @@ if [ "$status" == "1" ]; then
     docker logs mailcowdockerized_postfix-mailcow_1 --tail 30 -f
   fi
 fi
+
+cd MYPATH
+
 exit 0
