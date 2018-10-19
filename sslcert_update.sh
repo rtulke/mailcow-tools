5a14a986c8ff2e235c145aea42f620a9b0913fb4
Robert Tulke <rt@debian.sh>
2018-10-19 02:33:17 +0200

    Update sslcert_update.sh

--- sslcert_update.sh
+++ sslcert_update.sh
@@ -2,17 +2,31 @@
 
 MCDOMAIN="mx.foobar.com"
 LETSPATH="/etc/letsencrypt/live/"
+MPATH="/root/dev/mailcow-dockerized"        ## change here your mailcow-dockerized install path
+MCONF="mailcow.conf"
+MYPATH=$(pwd)
 
 echo "Do you wish to upgrade ssl certificates?"
 
 upgrade () {
 
   echo "start updating ssl certs"
-  mypath=$(pwd)
 
-  cd /root/dev/mailcow-dockerized
-  source mailcow.conf
-   
+  ## test mailconf exists
+  if [ ! -f "${MPATH}/${MCONF}" ] ; then
+    echo "file or directory not found, please verify the MPATH= and MCONF= variable";
+    exit 1
+  fi
+
+  ## test letsencrypt cert path exists
+  if [ ! -d "${LETSPATH}/${MCDOMAIN}" ] ; then
+    echo "file or directory not found, please verify the MPATH= and MCONF= variable";
+    exit 1
+  fi
+
+  cd $MPATH
+  source $MCONF
+    
   ## mv data/assets/ssl/*{,.bak.`date +%Y%m%d`}
   cp -a data/assets/ssl data/assets/ssl_old
  
@@ -22,7 +36,7 @@ upgrade () {
   # cp -L /etc/ssl/mail/dhparams.pem
 
   docker-compose restart
-  cd $mypath
+  cd $MYPATH
 
 }
 
