f9fd1ded9fd2786667269d7ec3123b828be766f6
Robert Tulke <rt@debian.sh>
2018-10-19 02:24:52 +0200

    Update sslcert_update.sh

--- sslcert_update.sh
+++ sslcert_update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+MCDOMAIN="mx.foobar.com"
+LETSPATH="/etc/letsencrypt/live/"
+
 echo "Do you wish to upgrade ssl certificates?"
 
 upgrade () {
@@ -9,13 +12,14 @@ upgrade () {
 
   cd /root/dev/mailcow-dockerized
   source mailcow.conf
-
-  #mv data/assets/ssl/*{,.bak.`date +%Y%m%d`}
+   
+  ## mv data/assets/ssl/*{,.bak.`date +%Y%m%d`}
   cp -a data/assets/ssl data/assets/ssl_old
-
-  cp -L /etc/letsencrypt/live/mx.rmail.org/cert.pem data/assets/ssl/cert.pem
-  cp -L /etc/letsencrypt/live/mx.rmail.org/privkey.pem data/assets/ssl/key.pem
-  cp -L /etc/ssl/mail/dhparams.pem
+ 
+  ## copy letsencrypts to mailcow container 
+  cp -L ${LETSPATH}/${MCDOMAIN}/cert.pem data/assets/ssl/cert.pem
+  cp -L ${LETSPATH}/${MCDOMAIN}/privkey.pem data/assets/ssl/key.pem
+  # cp -L /etc/ssl/mail/dhparams.pem
 
   docker-compose restart
   cd $mypath
