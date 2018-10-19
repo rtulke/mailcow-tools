52fada8f806154feb95810d5da87964ebd7dcad3
Robert Tulke <rt@debian.sh>
2018-10-19 02:29:34 +0200

    Update cleanup_after_upgrade.sh

--- cleanup_after_upgrade.sh
+++ cleanup_after_upgrade.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+MPATH="/root/dev/mailcow-dockerized"        ## change here your mailcow-dockerized install path
+MCONF="mailcow.conf"
+
+MYPATH=$(pwd)
+
+## test path/file
+if [ ! -f "${MPATH}/${MCONF}" ] ; then
+    echo "file or directory not found, please verify the MPATH= and MCONF= variable";
+    exit 1
+fi
+
+cd $MPATH
+source $MCONF
+
 echo
 docker ps -a -f status=running
 echo
@@ -13,3 +27,5 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+cd $MYPATH
